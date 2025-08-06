50bf28a89fc938b99733dc4a95afe5dbce5c876e
Aryan <125740477+Aryan140802@users.noreply.github.com>
2025-08-06 13:04:29 +0530

    Update tablespaces.py

--- tablespaces.py
+++ tablespaces.py
@@ -1,65 +1,28 @@
-TABLESPACE_QUERY = """
-WITH ts_data AS (
-  SELECT df.tablespace_name tspace,
-         ROUND(SUM(fs.bytes_free + fs.bytes_used) / 1024 / 1024, 2) tot_ts_size,
-         ROUND(SUM(fs.bytes_used) / 1024 / 1024, 2) used_ts_size,
-         ROUND(SUM(fs.bytes_free) / 1024 / 1024, 2) free_ts_size,
-         ROUND(SUM(fs.bytes_used) * 100 / SUM(fs.bytes_free + fs.bytes_used)) used_pct,
-         ROUND(SUM(fs.bytes_free) * 100 / SUM(fs.bytes_free + fs.bytes_used)) free_pct,
-         DECODE(SIGN(SUM(ROUND(((fs.bytes_free + fs.bytes_used) - fs.bytes_free) * 100 / (fs.bytes_free + fs.bytes_used))) - 80), 1, '!ALERT', '') warning
-  FROM SYS.V_$TEMP_SPACE_HEADER fs
-  JOIN dba_temp_files df ON fs.tablespace_name = df.tablespace_name AND fs.file_id = df.file_id
-  GROUP BY df.tablespace_name
+# Collect all results here
+all_results = {}
 
-  UNION ALL
+for config in db_configs:
+    db_name = config["name"]
+    try:
+        connection = oracledb.connect(
+            user=config["user"],
+            password=config["password"],
+            dsn=config["dsn"]
+        )
+        cursor = connection.cursor()
+        cursor.execute(query)
+        columns = [col[0].lower() for col in cursor.description]
+        rows = cursor.fetchall()
+        result = [dict(zip(columns, row)) for row in rows]
+        all_results[db_name] = result
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        all_results[db_name] = {
+            "error": str(e),
+            "traceback": traceback.format_exc()
+        }
 
-  SELECT df.tablespace_name tspace,
-         df.bytes / (1024 * 1024) tot_ts_size,
-         ROUND((df.bytes - SUM(fs.bytes)) / (1024 * 1024)) used_ts_size,
-         SUM(fs.bytes) / (1024 * 1024) free_ts_size,
-         ROUND((df.bytes - SUM(fs.bytes)) * 100 / df.bytes) used_pct,
-         ROUND(SUM(fs.bytes) * 100 / df.bytes) free_pct,
-         DECODE(SIGN(ROUND((df.bytes - SUM(fs.bytes)) * 100 / df.bytes) - 80), 1, '!ALERT', '') warning
-  FROM dba_free_space fs
-  JOIN (
-    SELECT tablespace_name, SUM(bytes) bytes
-    FROM dba_data_files
-    GROUP BY tablespace_name
-  ) df ON fs.tablespace_name = df.tablespace_name
-  GROUP BY df.tablespace_name, df.bytes
-
-  UNION ALL
-
-  SELECT tablespace_name tspace,
-         1 tot_ts_size,
-         1 used_ts_size,
-         0 free_ts_size,
-         100 used_pct,
-         0 free_pct,
-         '!' warning
-  FROM dba_data_files
-  GROUP BY tablespace_name
-  MINUS
-  SELECT tablespace_name tspace,
-         1 tot_ts_size,
-         1 used_ts_size,
-         0 free_ts_size,
-         100 used_pct,
-         0 free_pct,
-         '!' warning
-  FROM dba_free_space
-  GROUP BY tablespace_name
-)
-SELECT JSON_ARRAYAGG(
-         JSON_OBJECT(
-           'tablespace' VALUE tspace,
-           'tot_ts_size' VALUE tot_ts_size,
-           'used_ts_size' VALUE used_ts_size,
-           'free_ts_size' VALUE free_ts_size,
-           'used_pct' VALUE used_pct,
-           'free_pct' VALUE free_pct,
-           'warning' VALUE warning
-         )
-       ) AS tablespace_usage
-FROM ts_data
-"""
+# Write to tbs_output.json
+with open("tbs_output.json", "w") as f:
+    json.dump(all_results, f, indent=4)
