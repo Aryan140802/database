574e8f3180a4c155629b181a5e9b6e48cc66dfec
Aryan <125740477+Aryan140802@users.noreply.github.com>
2025-08-06 12:33:59 +0530

    Update tablespaces.py

--- tablespaces.py
+++ tablespaces.py
@@ -1,53 +1,63 @@
+SELECT JSON_ARRAYAGG(
+         JSON_OBJECT(
+           'tablespace' VALUE tspace,
+           'tot_ts_size' VALUE tot_ts_size,
+           'used_ts_size' VALUE used_ts_size,
+           'free_ts_size' VALUE free_ts_size,
+           'used_pct' VALUE used_pct,
+           'free_pct' VALUE free_pct,
+           'warning' VALUE warning
+         )
+       ) AS tablespace_usage
+FROM (
+  SELECT df.tablespace_name tspace,
+         ROUND(SUM(fs.bytes_free + fs.bytes_used) / 1024 / 1024, 2) tot_ts_size,
+         ROUND(SUM(fs.bytes_used) / 1024 / 1024, 2) used_ts_size,
+         ROUND(SUM(fs.bytes_free) / 1024 / 1024, 2) free_ts_size,
+         ROUND(SUM(fs.bytes_used) * 100 / SUM(fs.bytes_free + fs.bytes_used)) used_pct,
+         ROUND(SUM(fs.bytes_free) * 100 / SUM(fs.bytes_free + fs.bytes_used)) free_pct,
+         DECODE(SIGN(SUM(ROUND(((fs.bytes_free + fs.bytes_used) - fs.bytes_free) * 100 / (fs.bytes_free + fs.bytes_used))) - 80), 1, '!ALERT', '') warning
+  FROM SYS.V_$TEMP_SPACE_HEADER fs
+  JOIN dba_temp_files df ON fs.tablespace_name = df.tablespace_name AND fs.file_id = df.file_id
+  GROUP BY df.tablespace_name
 
-set lines 500
-set pages 100
-col tspace form a25 Heading "Tablespace"
-col tot_ts_size form 99999999 Heading "Size (Mb)"
-col free_ts_size form 99999999 Heading "Free (Mb)"
-col used_ts_size form 99999999 Heading "Used (Mb)"
-col used_pct form 99999 Heading "% Used"
-col free_pct form 99999 Heading "% Free"
-col warning form a10 Heading "Message"
-break on report
-compute sum label total of tot_ts_size on report
-compute sum label total of used_ts_size on report
-compute sum label total of free_ts_size on report
-         (select  df.tablespace_name tspace
-   ,       round(sum(fs.bytes_free + fs.bytes_used) / 1024 / 1024, 2) tot_ts_size
-   ,       round(sum(fs.Bytes_used) / 1024 / 1024, 2)  used_ts_size
-   ,       round(sum(fs.bytes_free) / 1024 / 1024, 2)  free_ts_size
-   ,       round(sum(fs.Bytes_used ) * 100 / sum((fs.bytes_free + fs.bytes_used))) used_pct
-   ,       round(sum(fs.Bytes_free ) * 100 / sum((fs.bytes_free + fs.bytes_used))) free_pct
-   ,      decode(sign(sum(round(((fs.bytes_free + fs.bytes_used)-fs.bytes_free)*100/(fs.bytes_free + fs.bytes_used))) - 80), 1, '
-   !ALERT', '') warning
-   from   SYS.V_$TEMP_SPACE_HEADER fs
-   ,      dba_temp_files df
-   where fs.tablespace_name(+) = df.tablespace_name
-     and fs.file_id(+) = df.file_id
-     group by df.tablespace_name
-     union
-     SELECT df.tablespace_name tspace
-     ,      df.bytes/(1024*1024) tot_ts_size
-     ,      round((df.bytes-sum(fs.bytes))/(1024*1024)) used_ts_size
-     ,      sum(fs.bytes)/(1024*1024) free_ts_size
-     ,      round((df.bytes-sum(fs.bytes))*100/df.bytes) used_pct
-     ,      round(sum(fs.bytes)*100/df.bytes) free_pct
-     ,      decode(sign(round((df.bytes-sum(fs.bytes))*100/df.bytes) - 80), 1, '!ALERT', '') warning
-     FROM dba_free_space fs
-     , (select tablespace_name, sum(bytes) bytes
-        from dba_data_files
-           group by tablespace_name
-              ) df
-              WHERE fs.tablespace_name(+) = df.tablespace_name
-              GROUP BY df.tablespace_name, df.bytes)
-              union
-              (select tablespace_name tspace,
-              1,1,0 free_ts_size,100 used_pct,0 free_pct,'!' warning from dba_data_files
-              group by tablespace_name
-              minus
-              select tablespace_name tspace,1,1,0 free_ts_size,100 used_pct,0 free_pct,'!' warning
-              from dba_free_space
-              group by tablespace_name)
-              order by 4
-              ;
+  UNION
 
+  SELECT df.tablespace_name tspace,
+         df.bytes / (1024 * 1024) tot_ts_size,
+         ROUND((df.bytes - SUM(fs.bytes)) / (1024 * 1024)) used_ts_size,
+         SUM(fs.bytes) / (1024 * 1024) free_ts_size,
+         ROUND((df.bytes - SUM(fs.bytes)) * 100 / df.bytes) used_pct,
+         ROUND(SUM(fs.bytes) * 100 / df.bytes) free_pct,
+         DECODE(SIGN(ROUND((df.bytes - SUM(fs.bytes)) * 100 / df.bytes) - 80), 1, '!ALERT', '') warning
+  FROM dba_free_space fs
+  JOIN (
+    SELECT tablespace_name, SUM(bytes) bytes
+    FROM dba_data_files
+    GROUP BY tablespace_name
+  ) df ON fs.tablespace_name = df.tablespace_name
+  GROUP BY df.tablespace_name, df.bytes
+
+  UNION
+
+  SELECT tablespace_name tspace,
+         1 tot_ts_size,
+         1 used_ts_size,
+         0 free_ts_size,
+         100 used_pct,
+         0 free_pct,
+         '!' warning
+  FROM dba_data_files
+  GROUP BY tablespace_name
+  MINUS
+  SELECT tablespace_name tspace,
+         1 tot_ts_size,
+         1 used_ts_size,
+         0 free_ts_size,
+         100 used_pct,
+         0 free_pct,
+         '!' warning
+  FROM dba_free_space
+  GROUP BY tablespace_name
+)
+ORDER BY free_ts_size;
